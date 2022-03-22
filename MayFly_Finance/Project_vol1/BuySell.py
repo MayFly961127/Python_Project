6fba068529ddb7f9e1740f544de6be722a9182ca
MayFly <70922690+MayFly961127@users.noreply.github.com>
2022-03-22 12:13:07 +0900

    Updated

--- MayFly_Finance/Project_vol1/BuySell.py
+++ MayFly_Finance/Project_vol1/BuySell.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas_datareader as pdr
 import yfinance as yfin
 import matplotlib.pyplot as plt
-taken = ['SOFI', 'ITUB']
+taken = ['SOFI', 'ITUB', 'BBD']
 yfin.pdr_override()
 start_date = '2022-01-01'
 df_brent = pdr.get_data_yahoo('BZ=F', start = start_date)
-df_SP = pdr.get_data_yahoo('^GSPC', start = start_date)
+df_SP = pdr.get_data_yahoo('^IXIC', start = start_date)
 df_SP = df_SP.reset_index()
 df_brent = df_brent.reset_index()
 df_brent_close = df_brent['Close']
@@ -18,9 +18,13 @@ SP_close = np.reshape(df_SP.to_numpy(), len(df_SP))
 RS_brent_SP = SP_close/brent_close
 x_data = np.linspace(0, len(SP_close), len(SP_close))
 date = np.reshape(df_date.to_numpy(),len(df_brent_close))
-plt.figure(figsize = (25,7))
+
+print(round(np.corrcoef(SP_close, brent_close)[0,1],2)*100, '%')
+print(brent_close[-1])
+#Plot brent VS Nasdaq
+plt.figure(figsize = (10,6))
 plt.plot(x_data, RS_brent_SP, 'g')
-plt.title('Brent and S&P')
+plt.title('Brent and Nasdaq')
 plt.xticks(x_data[::10], date[::10], rotation = 45)
 plt.show()
 for i in taken:
@@ -29,22 +33,26 @@ for i in taken:
   close = np.reshape(df_close.to_numpy(),len(df_close))
   RS_stock_SP = close/SP_close
 
-  o_2 = brent_close[-3] - brent_close[-2]
-  o_1 = brent_close[-2] - brent_close[-1]
-  t_2 = close[-3] - close[-2]
-  t_1 = close[-2] - close[-1]
-  oil_factor = -1*o_2*o_1
+  o_2 = brent_close[-2] - brent_close[-3]
+  o_1 = brent_close[-1] - brent_close[-2]
+  t_2 = close[-2] - close[-3]
+  t_1 = close[-1] - close[-2]
+  oil_factor = o_2*o_1
 
-  if t_2*t_1*(oil_factor) >= 0:
-    print('Buy more or keep: ' + str(i))
+  if oil_factor >= 0:
+    print('Not great: ' +  str(i))
+    print('The day before: ', t_2, '\nYesterday: ', t_1)
   else:
-    print('Sell a bit' + str(i))
-  plt.figure(figsize = (25,7))
+    if t_2 * t_1 >= 0:
+      print('Great: ' + str(i))
+    else:
+      print('Keep Eyes on' + str(i))
+  plt.figure(figsize = (10,6))
   plt.plot(x_data, close, 'r')
   plt.title('Close_stock:' + str(i))
   plt.xticks(x_data[::10], date[::10], rotation = 45)
   plt.show()
-  plt.figure(figsize = (25,7))
+  plt.figure(figsize = (10,6))
   plt.plot(x_data, RS_stock_SP, 'r')
   plt.title('RS stock with SP')
   plt.xticks(x_data[::10], date[::10], rotation = 45)
--- MayFly_Finance/Project_vol1/Trend_Dev.py
+++ MayFly_Finance/Project_vol1/Trend_Dev.py
@@ -3,70 +3,90 @@ import pandas as pd
 import yfinance as yfin
 import pandas_datareader as pdr
 import matplotlib.pyplot as plt
-stock = input("Enter the ACT symbol of the stock ")
-step = int(input("Enter the months for trend Dev "))
-step_sub = int(input("Enter the sub months for trend Dev "))
-print('step: ', step)
-print('step_sub ', step_sub)
-def MV(stock, step):
-    mv_step = step
-    mv_stock = []
-    n = len(stock)
-    i = 0
-    while i < n - mv_step + 1:
-        window_stock = stock[i: i + mv_step]
-        mv_stock.append(round(sum(window_stock) / mv_step, 5))
-        i += 1
-    return mv_stock
+def Trend_Dev(stock, step, step_sub, step_sub_sub):
+    # stock = input("Enter the ACT symbol of the stock ")
+    # step = int(input("Enter the months for trend Dev "))
+    # step_sub = int(input("Enter the sub months for trend Dev "))
+    # step_sub_sub = int(input("Enter the MV for the trend Dev "))
+    print('Trend_dv_dn ', step)
+    print('Trend_dv_nu ', step_sub)
+    print('Trend_dv_MV', step_sub_sub)
+    def MV(stock, step):
+        mv_step = step
+        mv_stock = []
+        n = len(stock)
+        i = 0
+        while i < n - mv_step + 1:
+            window_stock = stock[i: i + mv_step]
+            mv_stock.append(round(sum(window_stock) / mv_step, 5))
+            i += 1
+        return mv_stock
 
-yfin.pdr_override()
-df_SOFI = pdr.get_data_yahoo(stock)
-df_SOFI = df_SOFI.reset_index()
-h = len(df_SOFI)
-Date = df_SOFI['Date'].dt.strftime('%Y-%m-%d')
-Date = np.reshape(Date.to_numpy(),len(Date))
-df_SP = pdr.get_data_yahoo('^IXIC')
-df_SP = df_SP[-h:]
-df_SP = df_SP.reset_index()
-stock_close = df_SOFI['Close']
-sp_close = df_SP['Close']
-stock_close = np.reshape(stock_close.to_numpy(), len(stock_close))
-sp_close = np.reshape(sp_close.to_numpy(), len(sp_close))
-#This is what need
-rs_close = stock_close/sp_close
+    yfin.pdr_override()
+    df_SOFI = pdr.get_data_yahoo(stock, start = '1900-01-01')
+    df_SOFI = df_SOFI.reset_index()
+    Date_latest_stock = df_SOFI['Date']
+    Date_latest_stock = Date_latest_stock.to_numpy()[0]
+    df_SP = pdr.get_data_yahoo('^IXIC', start = Date_latest_stock)
+    df_SP = df_SP.reset_index()
+    if len(df_SOFI) > len(df_SP):
+        df_SOFI = df_SOFI[-len(df_SP):]
+        stock_close = df_SOFI['Close']
+        sp_close = df_SP['Close']
+        stock_close = np.reshape(stock_close.to_numpy(), len(df_SP))
+        sp_close = np.reshape(sp_close.to_numpy(), len(df_SP))
+    else:
+        df_SP = df_SP[-len(df_SOFI):]
+        stock_close = df_SOFI['Close']
+        sp_close = df_SP['Close']
+        stock_close = np.reshape(stock_close.to_numpy(), len(df_SOFI))
+        sp_close = np.reshape(sp_close.to_numpy(), len(df_SOFI))
+    #This is what need
+    rs_close = stock_close/sp_close
+    Date = df_SOFI['Date'].dt.strftime('%Y-%m-%d')
+    Date = np.reshape(Date.to_numpy(),len(Date))
 
-#MV
-step = step*30
-step_sub_de = (step_sub - 1)*30
-step_sub_in = (step_sub + 1)*30
-step_sub = step_sub*30
-MV_stock = MV(stock_close, step)
-m = len(MV_stock)
-trend_dev = stock_close[-m:]/MV_stock
-MV_3 = MV(stock_close, step_sub_de)
-MV_4 = MV(stock_close,step_sub)
-MV_5 = MV(stock_close, step_sub_in)
-MV_4 = MV_4[-m:]
-MV_3 = MV_3[-m:]
-MV_5 = MV_5[-m:]
-#plot the result
-Date_trend = Date[-m:]
-x_close = np.linspace(0,len(stock_close),len(stock_close))
-x_trend = x_close[-m:]
-plt.figure(figsize = (12,4))
-plt.plot(x_close, stock_close)
-plt.xlim(210,len(x_close))
-plt.xticks(x_close[::40], Date[::40], rotation = 45)
-plt.title(stock)
-plt.show()
-#plot the result
-fig, ax = plt.subplots(figsize = (12,4))
-ax.plot(x_trend, trend_dev, 'ko' ,label = 'Trend_Dev')
-ax2 = ax.twinx()
-ax2.plot(x_trend,MV_4, color = 'r', label = 'MV_' + str(step_sub/30))
-ax2.plot(x_trend, MV_3, color = 'b', label = 'MV_'+ str((step_sub_de)/30))
-ax2.plot(x_trend, MV_5, color = 'g', label = 'MV_'+ str((step_sub_in)/30))
-plt.xticks(x_trend[::20], Date_trend[::20], rotation = 45)
-plt.title('Trend Deviation')
-plt.legend()
-plt.show()
+    #MV
+    step = step*30
+    step_sub = step_sub*30
+    step_sub_sub = step_sub_sub*30
+    Trend_dv_de = MV(stock_close, step)
+    Trend_dv_de = np.array(Trend_dv_de)
+    m = len(Trend_dv_de)
+    Trend_dv_nu = MV(stock_close, step_sub)
+    Trend_dv_nu = Trend_dv_nu[-m:]
+    Trend_dv_nu = np.array(Trend_dv_nu)
+    Trend_dv_MV = MV(stock_close, step_sub_sub)
+    Trend_dv_MV = Trend_dv_MV[-m:]
+
+    Trend_dv = Trend_dv_de/Trend_dv_nu
+    #plot the stock
+    Date_trend = Date[-m:]
+    x_close = np.linspace(0,len(Date_trend),len(Date_trend))
+    x_trend = x_close[-m:]
+    plt.figure(figsize = (12,4))
+    plt.plot(x_close, stock_close[-m:])
+    split = int(len(x_close)**(3/4))
+    plt.xticks(x_close[::split], Date_trend[::split], rotation = 45)
+    plt.title(stock)
+    plt.grid()
+    plt.show()
+    #plot the RS
+    plt.figure(figsize = (12,4))
+    plt.plot(x_close, rs_close[-m:])
+    plt.xticks(x_close[::split], Date_trend[::split], rotation = 45)
+    plt.title('RS with nasdaq: ' + str(stock))
+    plt.grid()
+    plt.show()
+
+    #plot the result
+    fig, ax = plt.subplots(figsize = (12,4))
+    ax.plot(x_trend, Trend_dv, '-r' ,label = 'Trend_Dev')
+    ax2 = ax.twinx()
+    ax2.plot(x_trend,Trend_dv_MV, '--k', label = 'MV_' + str(step_sub_sub/30))
+    split_trend = int(len(x_trend)**(3/4))
+    ax.set_xticks(x_trend[::split_trend])
+    ax.set_xticklabels(Date_trend[::split_trend], rotation = 45)
+    plt.title('Trend Deviation')
+    fig.legend()
+    plt.show()
